4366d3f812a96bc318c501232f87fad4b235a898
talmudP <talmudDNM@gmail.com>
2021-11-11 10:36:48 +0900

    Initial commit

---
+++ src/main/java/por/dnmpor/DnmPorApplication.java
@@ -0,0 +1,13 @@
+package por.dnmpor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnmPorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnmPorApplication.class, args);
+    }
+
+}
